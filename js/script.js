a416df6c5a4610115773aad88a1ccc8333c707a0
Mohammad Shahin <74470655+shahin-sobuj@users.noreply.github.com>
2020-11-27 03:24:15 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+
+        items:1,
+        margin:10,
+        nav:true,
+        responsive:Object,
+        autoplay:true,
+        autoplayTimeout:3000, 
+        loop:true,
+    });
+  });
